dd67805079fe93b2e35ce700e614b1852f52c05c
530154436 <530154436@qq.com.com>
2019-01-14 00:01:27 +0800

    更新第二章

---
+++ utility/animation_sin.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+# Filename: sin.py
+# Author：hankcs
+# Date: 2015/1/30 22:41
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+# first set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+line, = ax.plot([], [], lw=2)
+
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+
+# animation function.  this is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+
+# call the animator.  blit=true means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
+
+plt.show()
---
+++ 第2章 感知器模型(Perceptron)/perceptron1.py
@@ -0,0 +1,74 @@
+# /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# 参数向量
+weights = []
+
+# 偏置
+b = 0
+
+print_pattern = 'Iteration {}, 误分类点: x{}, w={}, b={}'
+
+def update(item, learning_rate = 1):
+    '''
+    更新权值、偏置
+    :param item: 实例点及类别 [(x1,x2),y]
+    :return:
+    '''
+    global weights, b
+    for i in range(len(item)):
+        weights[i] += learning_rate * item[1] * item[0][i]
+    b += learning_rate * item[1]
+
+def cal(item):
+    '''
+    计算 yi*(w*xi + b) <= 0
+    :param item:
+    :return:
+    '''
+    res = 0
+    for i in range(len(item[0])):
+        res += item[0][i] * weights[i]
+    res += b
+    res *= item[1]
+    return res
+
+def check(iteration, learning_rate = 1):
+    # 标识是否存在误分类点
+    flag = False
+    for xi in range(len(data_set)):
+        if cal(data_set[xi]) <= 0:
+            flag = True
+            update(data_set[xi], learning_rate=learning_rate)
+            print(print_pattern.format(iteration, xi + 1, weights, b))
+    # 误分类点不存在，迭代结束
+    return flag
+
+def train(data_set, max_iteration=1000, learning_rate=1):
+    '''
+    感知机学习算法的原始形式
+    :param data_set:
+    :return:
+    '''
+    for i in range(len(data_set)):
+        weights.append(0)
+    iteration = 0
+    print(print_pattern.format(iteration, 1, weights, b))
+    while True:
+        if not check(iteration) or iteration>max_iteration:
+            break
+        iteration += 1
+
+if __name__ == '__main__':
+    # 训练集
+    data_set = [[(3, 3), 1], [(4, 3), 1], [(1, 1), -1]]
+    train(data_set)
+
+
+
+
+
+
+
+
+
